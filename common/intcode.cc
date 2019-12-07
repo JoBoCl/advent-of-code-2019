2cab3bc02b5259039b4b1e2f5283ded4dc00e7ce
Joshua Clark <joshua@clark.io>
2019-12-07 14:59:52 +0100

    Refactor intcode out to a common file

---
+++ common/intcode.cc
@@ -0,0 +1,138 @@
+#include "./intcode.h"
+#include <iostream>
+#include <map>
+#include <vector>
+
+int IntCode::pow(int n) {
+  int v = 1;
+  while (n-- > 0) {
+    v *= 10;
+  }
+  return v;
+}
+
+bool IntCode::reference(int arg) {
+  return 0 == ((program[position] / pow(arg + 1)) % 10);
+}
+
+Operation IntCode::op() {
+  assert(position < program.size());
+  int inst = program[position] % 100;
+  assert((1 <= inst && inst <= 8) || inst == 99);
+  return static_cast<Operation>(inst);
+}
+
+int IntCode::argument(int number) {
+  return value(position + number, reference(number));
+}
+
+int IntCode::value(int address, bool reference) {
+  if (reference) {
+    return program[program[address]];
+  }
+  if (address >= program.size()) {
+    return additionalMemory[address];
+  }
+  return program[address];
+}
+
+void IntCode::store(int address, int value) {
+  if (address >= program.size()) {
+    additionalMemory[address] = value;
+  }
+  program[address] = value;
+}
+
+IntCode::IntCode(std::vector<int>& _program) : program(_program) {}
+
+void IntCode::advance() {
+  if (shouldAdvance) {
+    int jump = OPERATION_SIZE[lastOp];
+    assert(position + jump < program.size());
+    position += jump;
+  }
+}
+
+std::optional<int> IntCode::exec() {
+  assert(!waitingForInput);
+  lastOp = op();
+  shouldAdvance = true;
+  switch (lastOp) {
+    case ADD: {
+      int arg1 = argument(1);
+      int arg2 = argument(2);
+      int output = value(position + 3, false);
+      store(output, arg1 + arg2);
+      break;
+    }
+    case MUL: {
+      int arg1 = argument(1);
+      int arg2 = argument(2);
+      int output = value(position + 3, false);
+      store(output, arg1 * arg2);
+      break;
+    }
+    case SET: {
+      waitingForInput = true;
+      break;
+    }
+    case OUT: {
+      return argument(1);
+    }
+    case JIT: {
+      int arg1 = argument(1);
+      int arg2 = argument(2);
+      if (arg1 != 0) {
+        assert(arg2 < program.size());
+        position = arg2;
+        shouldAdvance = false;
+      }
+      break;
+    }
+    case JIF: {
+      int arg1 = argument(1);
+      int arg2 = argument(2);
+      if (arg1 == 0) {
+        assert(arg2 < program.size());
+        position = arg2;
+        shouldAdvance = false;
+      }
+      break;
+    }
+    case SLT: {
+      int arg1 = argument(1);
+      int arg2 = argument(2);
+      int output = value(position + 3, false);
+      store(output, arg1 < arg2 ? 1 : 0);
+      break;
+    }
+    case SEQ: {
+      int arg1 = argument(1);
+      int arg2 = argument(2);
+      int output = value(position + 3, false);
+      store(output, arg1 == arg2 ? 1 : 0);
+      break;
+    }
+    case FIN:
+      break;
+  }
+  return {};
+}
+
+bool IntCode::waiting() { return waitingForInput; }
+
+void IntCode::input(int value) {
+  int output = program[position + 1];
+  program[output] = value;
+  waitingForInput = false;
+}
+
+bool IntCode::stopped() { return position > program.size() || lastOp == FIN; }
+
+void IntCode::print() {
+  for (int i = 0; i < program.size(); i++) {
+    std::cout << (i == position ? "(" : "") << program[i]
+              << (i == position ? ")" : "") << ", ";
+  }
+  std::cout << std::endl;
+}
---
+++ common/intcode.h
@@ -0,0 +1,53 @@
+#ifndef __COMMON_INTCODE
+#define __COMMON_INTCODE
+#include <map>
+#include <vector>
+#include <iostream>
+
+enum Operation {
+  ADD = 1,
+  MUL = 2,
+  SET = 3,
+  OUT = 4,
+  JIT = 5,
+  JIF = 6,
+  SLT = 7,
+  SEQ = 8,
+  FIN = 99,
+};
+
+class IntCode {
+ private:
+  std::map<Operation, int> OPERATION_SIZE{{ADD, 4}, {MUL, 4}, {SET, 2},
+                                          {OUT, 2}, {FIN, 0}, {JIT, 3},
+                                          {JIF, 3}, {SLT, 4}, {SEQ, 4}};
+  std::vector<int>& program;
+  std::map<int, int> additionalMemory;
+  Operation lastOp = SET;
+  bool shouldAdvance = true;
+  int position = 0;
+  bool waitingForInput = false;
+
+  int pow(int n); 
+  bool reference(int arg); 
+  Operation op(); 
+  int argument(int number); 
+  int value(int address, bool reference); 
+  void store(int address, int value);
+
+ public:
+  IntCode(std::vector<int>& _program);
+
+  void advance();
+
+  std::optional<int> exec();
+
+  bool waiting();
+
+  void input(int value);
+
+  bool stopped();
+
+  void print();
+};
+#endif
