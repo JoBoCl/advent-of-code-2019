fb0a3f25dec32148abb8da21eb255690bebec6d0
Joshua Clark <joshua@clark.io>
2019-12-15 19:38:24 +0000

    Day 14 part 1

--- 14/fourteen.cc
+++ 14/fourteen.cc
@@ -1,26 +1,32 @@
 #include <cassert>
+#include <cmath>
 #include <iostream>
 #include <map>
 #include <regex>
 #include <string>
+#include <string_view>
 #include <vector>
+
 #include "../common/parser.h"
+
+using std::literals::operator""sv;
 
 std::regex component("([0-9]+) ([A-Z]+)[, ]*");
 
-using Chemicals = std::map<std::string, int>;
+using Chemicals = std::map<std::string, long>;
+using Chemical = std::pair<std::string, long>;
 
 struct Reaction {
-  const Chemicals inputs;
-  const Chemicals outputs;
+  Chemicals inputs;
+  Chemical output;
 
-  Reaction(const Chemicals i, const Chemicals o) : inputs(i), outputs(o) {}
+  Reaction() : inputs(Chemicals{}), output(std::make_pair<>("", 0L)) {}
+  Reaction(Chemicals i, Chemical o) : inputs(i), output(o) {}
 };
 
 std::optional<Reaction> extractReaction(const std::string& s) {
   Chemicals inputs;
-  Chemicals outputs;
-  auto arrow = s.find("=> ");
+  size_t arrow = s.find("=> ");
 
   // Valid reactions contain a set of chemicals on the left and right hand side.
   if (arrow == std::string::npos) {
@@ -29,52 +35,72 @@ std::optional<Reaction> extractReaction(const std::string& s) {
 
   std::smatch componentMatch;
   auto from = s.begin();
-  auto arrowStart = s.begin();
-  std::advance(arrowStart, arrow);
+  auto arrowStart = s.begin() + arrow;
   bool match = false;
 
   do {
     match = std::regex_search(from, arrowStart, componentMatch, component);
     if (match) {
-      std::cout << componentMatch[0] << "\n";
-      inputs[componentMatch[2]] = std::stoi(componentMatch[1]);
+      inputs[componentMatch[2]] = std::stol(componentMatch[1]);
       std::advance(from, componentMatch[0].second - componentMatch[0].first);
     } else {
       from++;
     }
   } while (from != arrowStart && match);
-  std::cout << "\n";
-  do {
-    match = std::regex_search(from, s.end(), componentMatch, component);
-    if (match) {
-      std::cout << componentMatch[0] << "\n";
-      outputs[componentMatch[2]] = std::stoi(componentMatch[1]);
-      std::advance(from, componentMatch[0].second - componentMatch[0].first);
-    } else {
-      from++;
-    }
-  } while (from != s.end());
-  std::cout << "\n";
-  std::cout << "\n";
 
-  return Reaction(inputs, outputs);
+  match = std::regex_search(from, s.end(), componentMatch, component);
+  return Reaction(
+      inputs, std::make_pair(componentMatch[2], std::stol(componentMatch[1])));
 }
 
-Reaction findProducingReactions(const std::vector<Rection>& reactions,
-                                const std::string& chemical = "FUEL") {}
+/** Every chemical is produced by exactly one reaction. */
+Reaction& findProducingReactions(std::vector<Reaction>& reactions,
+                                 std::string& chemical) {
+  auto it = std::find_if(
+      reactions.begin(), reactions.end(),
+      [chemical](Reaction& r) { return r.output.first == chemical; });
+  return *it;
+}
 
-std::vector<Reaction> findOriginatingReactions(
-    const std::vector<Rection>& reactions,
-    const std::string& chemical = "ORE") {
-  return std::find_if(reactions.start(), reactions.end(), [chemical](const Reaction& r) {
-    return r.outputs.count(chemical) == 1;
-                  });
+/** Returns the cost of making the chemical (if any), and adds the chemical to
+ * the store, and removes the components that were required. */
+long makeChemical(std::vector<Reaction>& reactions,
+                  std::map<std::string, long>& resources, std::string chemical,
+                  long quantity, bool debug = false) {
+  if (debug) {
+    std::cout << "Producing: " << quantity << " * " << chemical << "\n";
+  }
+  if (chemical == "ORE") return quantity;
+  // We've got some of the chemical, so don't bother making all of it.
+  long available = resources[chemical];
+  if (available >= quantity) {
+    return 0L;
+  }
+  Reaction& reaction = findProducingReactions(reactions, chemical);
+  long outputQuantity = reaction.output.second;
+  // Can't have partial reactions, so must perform at least one.
+  long reactionsRequired =
+      std::ceil((double)(quantity - available) / outputQuantity);
+  long total = 0L;
+  for (auto& [k, v] : reaction.inputs) {
+    total +=
+        makeChemical(reactions, resources, k, v * reactionsRequired, debug);
+    resources[k] -= v * reactionsRequired;
+  }
+  resources[reaction.output.first] += outputQuantity * reactionsRequired;
 
+  return total;
 }
 
 int main(int argc, char** argv) {
   Parser p(argc == 1 ? "fourteen.aoc" : argv[1]);
-  std::vector<Reaction> reactions = p.parseLines<Reaction>(extractReaction);
+  auto reactions = p.parseLines<Reaction>(extractReaction);
+  std::map<std::string, long> store;
+
+  std::cout << "Part One: "
+            << makeChemical(reactions, store, "FUEL", 1L,
+                            argc == 3 && argv[2] == "debug"sv)
+            << "\n";
 
   return 0;
 }
